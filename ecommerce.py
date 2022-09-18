5543940efa47b06f0bdfa81eb01bf32d0035aedb
lhaislla <lhaislla@gmail.com>
2022-09-18 13:58:15 -0300

    correção do erro

--- ecommerce.py
+++ ecommerce.py
@@ -4,7 +4,15 @@ import numpy as np  # np mean, np random
 import pandas as pd  # read csv, df manipulation
 import plotly.express as px  # interactive charts
 import streamlit as st  # 🎈 data web app development
-from graficos import *
+#from graficos import *
+
+
+#Comando de execução do streamlit
+st.set_page_config(
+    page_title="Dashboard E-commerce Olist",
+    page_icon="chart_with_upwards_trend",
+    layout="wide",
+    initial_sidebar_state="expanded")
 
 # read csv from DW
 questao1 = pd.read_csv('data/questao_1.csv')
@@ -21,6 +29,79 @@ st.subheader('Este projeto apresenta o resultado da modelagem do DW gerado por m
 with st.sidebar:
     st.header('Modelagem de Dados - Grupo 1')
     opcoes_arq = st.selectbox("Opções de Consultas:",('Pedidos em datas comemorativas','Pedidos por dia da semana','Avaliação dos produtos por categorias','Forma de pagamento do pedido por estado','Categoria do pedido por estação do ano'))
+# Organizando graficos lado a lado por meio de colunas
+placeholder = st.empty() # criando um  container para as metricas
+with placeholder.container():
+    kpi1, kpi2, kpi3 = st.columns(3)
+    fig_col1, fig_col2 = st.columns(2)
+    fig_col3, fig_col4 = st.columns(2)
+    fig_col5, fig_col6= st.columns(2)
+
+#Metricas e indicadores
+def metrica1(kpi1,value,delta,label):
+    metrica1 = kpi1.metric(
+            label1=label,
+            value = value,
+            delta = delta,)
+    return metrica1 
+
+def metrica2(kpi2,value,delta,label):
+    metrica2 = kpi2.metric(
+            label2=label,
+            value=value,
+            delta=delta,)
+    return metrica2
+
+def metrica3(kpi3,value,delta,label):
+    metrica3 = kpi3.metric(
+    label3=label,
+            value=value,
+            delta=delta,)
+    return metrica3
+
+
+#Graficos
+def density_heatmap(fig_col1,df,x,y):
+    with fig_col1:
+        st.markdown("### Density Heatmap")
+        fig = px.density_heatmap(df, y, x)
+        return st.write(fig)
+
+def histogram(fig_col2,df,x,y):  
+    with fig_col2:
+        st.markdown("###  Histogram")
+        fig2 = px.histogram(df,x,y)
+        return st.write(fig2)
+
+def bar_chart(fig_col3,df, x,y):
+    with fig_col3:
+        st.markdown("###  Bar Chart")
+        fig3 =  px.bar(df, y, x) 
+        return st.write(fig3)
+
+def  scatter_chart(fig_col4, df ,x, y):   
+    with fig_col4:
+        st.markdown("### Scatter Chart")
+        fig4 =  px.scatter(df, y, x) 
+        return st.write(fig4)
+
+
+def line_chart(fig_col5, df ,y):
+    with fig_col5:
+        st.markdown("### line Chart")
+        fig5 = px.line(df, y)
+        return st.write(fig5)
+
+def pie_chart(fig_col6, df ,x, y):
+    with fig_col6:
+        st.markdown("###  Pie Chart")
+        fig6 = px.pie(df, values=y, names=x)
+        return st.write(fig6)
+
+def box_chart(df,x,y):
+    st.markdown("### Box Chart")
+    fig7 = px.box(df, x=x,y=y)
+    return st.write(fig7)
     
 #Condições dos Filtros
 if opcoes_arq == 'Pedidos por dia da semana':
@@ -74,7 +155,7 @@ elif opcoes_arq == 'Avaliação dos produtos por categorias':
     color = df["categoria_produto"]
 
     #Chamada dos gráficos
-    density_heatmap(fig_col1,df,x,y)
+    #density_heatmap(fig_col1,df,x,y)
     histogram(fig_col2,df,x,y)
     bar_chart(fig_col3,df, x,y)
     scatter_chart(fig_col4, df ,x, y)
