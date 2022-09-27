ef03365502cf4b6ee4d9289d4acb1873c41cda57
jonathasfsilva <jonathas_ssilva@hotmail.com>
2022-09-26 21:18:58 -0300

    streamlit mysql conection

---
+++ pets.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE pets;
+
+USE pets;
+
+CREATE TABLE mytable (
+    name            varchar(80),
+    pet             varchar(80)
+);
+
+INSERT INTO mytable VALUES ('Mary', 'dog'), ('John', 'cat'), ('Robert', 'bird');
